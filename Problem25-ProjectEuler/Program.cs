4e661630d4678112e6e2d0f488ed5d6be9d67e61
nikil <nikil@DESKTOP-73MHGNO>
2022-01-06 18:25:39 -0500

    Add project files.

---
+++ Problem25-ProjectEuler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+
+
+BigInteger currentNum = 0;
+BigInteger previousNum = 2;
+BigInteger lastNum = 1;
+BigInteger index = 3;
+
+while (true)
+{
+
+    currentNum = lastNum + previousNum;
+    index++;
+    if (currentNum.ToString().Length >= 1000)
+        break;
+    lastNum = previousNum;
+    previousNum = currentNum;
+}
+
+Console.WriteLine(index);
